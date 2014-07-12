cd7b92ceebbfd166f8982e2c16e59e015342e7fb
Cody Piersall <cody.piersall@gmail.com>
2014-07-12 13:58:09 -0500

    Move sqrt functionality into another module.

--- exercises/chapter1/1.6.scm
+++ exercises/chapter1/1.6.scm
@@ -15,33 +15,14 @@
 (new-if (= 1 1) 0 5)
 ; 0
 
-; these are functions that the sqrt-iter program needs.
-
-(define (square x) (* x x))
-
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
-
-(define (average x y)
-  (/ (+ x y) 2))
-
-(define (improve guess x)
-  (average guess (/ x guess)))
-
+(load "sqrt.scm")
 
 ; Delighted, Alyssa uses new-if to rewrite the square-root program:
-
 (define (sqrt-iter-new guess x)
   (display "here")
   (new-if (good-enough? guess x)
           guess
           (sqrt-iter-new (improve guess x) x)))
-
-; this is the original sqrt-iter program.
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
-          guess
-          (sqrt-iter (improve guess x) x)))
 
 ; What happens when Alyssa attempts to use this to compute square roots?
 ; Explain.
