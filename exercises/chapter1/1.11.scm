c3296b512ee751df51f40490cc6cc73269031579
Cody Piersall <cody.piersall@gmail.com>
2014-07-13 23:29:11 -0500

    Write iterative procedure.

--- exercises/chapter1/1.11.scm
+++ exercises/chapter1/1.11.scm
@@ -3,6 +3,8 @@
 ; computes f by means of a recursive process. Write a procedure that computes f
 ; by means of an iterative process.
 
+; The recursive process is easy; just translate the definition into Scheme.
+; Not a problem.
 (define (rec-f n)
   (if (< n 3)
       n
@@ -10,5 +12,33 @@
          (* 2 (rec-f (- n 2)))
          (* 3 (rec-f (- n 3)))
       )
+  )
+)
+
+; Iterating is a lot trickier.  Let's write down the first few values to get a
+; feel for the pattern.
+;
+; 0 1 2 4 11 25
+;
+; Actually, just writing the first couple out, I realized that I have to keep
+; track of only the last three numbers in the sequence.  Those fully define the
+; state of the program.
+;
+; we should be able to tell how
+
+(define (iter-f a)
+  (define (iterate n-3 n-2 n-1 count)
+    (define this-one (+ n-1
+                        (* 2 n-2)
+                        (* 3 n-3))
+    )
+    (if (= count a)
+        this-one
+        (iterate n-2 n-1 this-one (+ count 1))
+    )
+  )
+  (if (< a 3)
+      a
+      (iterate 0 1 2 3)
   )
 )
