748c14e706dc68569519a73f0c9f96e8f4ec4887
Cody Piersall <cody.piersall@gmail.com>
2014-08-11 22:22:44 -0500

    Do problem 1.16.  It's pretty good.

--- exercises/chapter1/1.16.scm
+++ exercises/chapter1/1.16.scm
@@ -1,6 +1,6 @@
 ; Exercise 1.16.  Design a procedure that evolves an iterative exponentiation
 ; process that uses successive squaring and uses a logarithmic number of steps,
-; as does fast-expt. (Hint: Using the observation that b^(n/2)^2 = b^2)^n/2,
+; as does fast-expt. (Hint: Using the observation that b^(n/2)^2 = (b^2)^n/2,
 ; keep, along with the exponent n and the base b, an additional state variable
 ; a, and define the state transformation in such a way that the product a bn is
 ; unchanged from state to state. At the beginning of the process a is taken to
@@ -9,3 +9,17 @@
 ; unchanged from state to state is a powerful way to think about the design of
 ; iterative algorithms.)
 
+(define (even x) (= 0 (modulo x 2)))
+(define (squared x) (* x x))
+(define (halved x) (/ x 2))
+
+(define (pow base exp_)
+  (define (pow-iter base exp_ counter)
+    (cond
+      ((= exp_ 1) (* base counter))
+      ((even exp_) (pow-iter (squared base) (halved exp_) counter))
+      (else (pow-iter base (- exp_ 1) (* counter base)))
+    )
+  )
+  (pow-iter base exp_ 1)
+)
