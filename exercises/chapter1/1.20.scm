7e75e19aa50800ee998e5a737355a96162f49917
Cody Piersall <cody.piersall@gmail.com>
2014-08-30 22:17:59 -0500

    Finish exercise 1.20; I had mixed up applicative-order vs. normal-order evaluation.

--- exercises/chapter1/1.20.scm
+++ exercises/chapter1/1.20.scm
@@ -14,16 +14,111 @@
 ; the normal-order evaluation of (gcd 206 40)? In the applicative-order
 ; evaluation?
 
-; normal order:
-(gcd 204 40)
-; ->
-(gcd 40 (remainder 204 40)) ; (remainder 204 40) == 4
-(gcd 40 4)
-; ->
-(gcd 40 (remainder 4 40)) ; (remainder 4 40) == 0
-(gcd 4 0)
-4
+; Applicative order:
+(gcd 206 40)
 
-; 2 remainder operations are performed
+(gcd 40 (remainder 206 40)) ; (remainder 206 40) == 6
+(gcd 40 6)
 
+(gcd 6 (remainder 40 6)) ; (remainder 6 40) == 4
+(gcd 6 4)
 
+(gcd 4 (remainder 6 4)) ; (remainder 4 6) == 2
+(gcd 4 2)
+
+(gcd 2 (remainder 4 2)) ; (remainder 4 2) == 0
+(gcd 2 0) ; -> 2
+
+; For applicative order, remainder is applied 4 times.
+
+;;;;;;;;;;;;;;;;;;;;;
+
+; Normal order
+; I'm going to fully expand this and hopefully not regret it.
+(gcd 206 40)
+(if (= 40 0)
+    40
+    (gcd 40 (remainder 206 40)))
+
+;;;;;
+(gcd 40 (remainder 206 40))
+; substituting, we get
+(if (= (remainder 206 40) 0)
+    40
+    (gcd (remainder 206 40) (remainder 40 (remainder 206 40))))
+
+; We've got to evaluate the one remainder now (= (remainder 204 40))
+; (if (= 6 0)
+; 40
+; (gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+
+; We've done *1* remainder operation; now we have to plug in some more numbers.
+
+(gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+
+; reduces to
+
+(if (= (remainder 40 (remainder 204 40)) 0)
+ (remainder 206 40)
+ (gcd
+  (remainder 40 (remainder 206 40)) ; a
+  (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))) ; b
+
+; we have to do 2 more remainder operations so the branch knows which one to
+; take.  (remainder 40 (remainder 204 40)) does not equal 0; keep going.
+
+; 3 remainder operations have been done total now.
+
+(if (= (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) 0)
+  (remainder 40 (remainder 206 40))
+  (gcd
+    (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) ; a
+    (remainder
+      (remainder 40 (remainder 206 40))
+      (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+    ) ; b
+  )
+)
+
+; okay, well I'm confused.  Maybe I haven't lost anything yet.
+
+; 4 more remainder operations done in the if statement; still evaluates false.
+; 7 total remainder operations done now.
+; Lots of stuff to substitiute now.
+
+; Need to reduce this expression:
+(gcd
+    (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) ; a
+    (remainder
+      (remainder 40 (remainder 206 40))
+      (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+    ) ; b
+  )
+
+(if (=
+       (remainder
+         (remainder 40 (remainder 206 40))
+         (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+       0)
+    (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+    (gcd (remainder
+           (remainder 40 (remainder 206 40))
+           (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+         )
+         (remainder (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+                    (remainder
+                      (remainder 40 (remainder 206 40))
+                      (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+    ))))
+
+; Honestly I have no idea if I substituted this correctly.  Lots of yanking in
+; vim; hopefully it worked out.
+
+; Evaluating the equality expression makes 7 more remainder operations take
+; place.  Finally, the equality evaluates to true.  4 more remainder operations
+; are done to evaluate the if-true branch.
+
+; In total, 18 remainder operations were made.  It was a terrible time for
+; everyone.
+
+; My answer matches Bill the Lizard's, so probably I did it right.
