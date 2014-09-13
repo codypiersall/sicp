1d146f8d14537834e655ab660712e7d3628f5ec0
Cody Piersall <cody.piersall@gmail.com>
2014-09-13 15:59:16 -0500

    Finish exercise 1.31.scm

--- exercises/chapter1/1.31.scm
+++ exercises/chapter1/1.31.scm
@@ -14,9 +14,46 @@
 ; generates an iterative process. If it generates an iterative process, write
 ; one that generates a recursive process.
 
-; recursive process:
+; Write a procedure called product that returns the product of the values of a
+; function at points over a given range.
 (define (product term a next b)
   (if (> a b)
       1
       (* (term a)
          (product term (next a) next b))))
+
+; Show how to define factorial in terms of product.
+(define (inc x) (+ x 1))
+(define (factorial n) (product2 identity 1 inc n))
+
+; Also use product to compute approximations to pi using the
+; formula
+;
+; pi    2 * 4 * 4 * 6 * 6 * 8 ...
+; -- = --------------------------
+; 4     3 * 3 * 5 * 5 * 7 * 7 ...
+(define (even x) (= 0 (modulo x 2)))
+(define (pi n)
+  (define (numerator)
+    (define (numterm k)
+      (if (even k)
+          (+ k 2)
+          (+ k 3)))
+    (product2 numterm 0 inc (- n 1)))
+  (define (denominator)
+    (define (denterm k)
+      (if (even k)
+        (+ k 3)
+        (+ k 2)))
+    (product2 denterm 0 inc (- n 1)))
+  (* 4 (/ (numerator) (denominator))))
+
+; iterative
+(define (product2 term a next b)
+  (define (iter x result)
+    (if (> x b)
+        result
+        (iter (next x) (* (term x) result))))
+  (iter a 1))
+
+
